6a73760cd68f514d7b1fc61c35e20b2d90cad4d8
mcduffie.michael <mcduffie.michael@dc4ab8e2-e685-11de-93f0-cd01e99e34ab>
2010-02-08 01:44:18 +0000

    Adding drop down module to SVN.
    git-svn-id: https://segex.googlecode.com/svn/trunk@14 dc4ab8e2-e685-11de-93f0-cd01e99e34ab

---
+++ SGX/DropDownData.pm
@@ -0,0 +1,80 @@
+=head1 NAME
+
+SGX::DropDownData
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+Object to populate drop down boxes.
+
+=head1 AUTHORS
+Michael McDuffie
+
+=head1 SEE ALSO
+
+
+=head1 COPYRIGHT
+
+
+=head1 LICENSE
+
+Artistic License 2.0
+http://www.opensource.org/licenses/artistic-license-2.0.php
+
+=cut
+
+package SGX::DropDownData;
+
+use strict;
+use warnings;
+
+sub new {
+	# This is the constructor
+	my $class = shift;
+
+	my $self = {
+		_dbh		=> shift,
+		_LoadQuery	=> shift,
+		_dropDownList	=> {},
+		_dropDownValue	=> ()
+	};
+
+	bless $self, $class;
+	return $self;
+}
+
+sub loadDropDownValues
+{
+	my $self		= shift;
+	my $dataQuery		= shift;
+
+	#Temp variables used to create the hash and array for the dropdown.
+	my %tempLabel;
+	my @tempValue;
+
+	#Variables used to temporarily hold the database information.
+	my $tempRecords		= '';
+	my $tempRecordCount	= '';
+	my @tempRecordsArray	= '';
+
+	$tempRecords 		= $self->{_dbh}->prepare($self->{_LoadQuery}) or die $self->{_dbh}->errstr;
+	$tempRecordCount	= $tempRecords->execute or die $self->{_dbh}->errstr;
+
+	#Grab all platforms and build the hash and array for drop down.
+	@tempRecordsArray	= @{$tempRecords->fetchall_arrayref};
+
+	foreach (sort {$a->[0] cmp $b->[0]} @tempRecordsArray)
+	{
+		$tempLabel{$_->[0]} = $_->[1];
+		push(@tempValue,$_->[0]);
+	}
+
+	#Assign members variables reference to the hash and array.
+	$self->{_dropDownList} 		= \%tempLabel;
+	$self->{_dropDownValue}		= \@tempValue;
+	
+	#Finish with database.
+	$tempRecords->finish;	
+}
+
+1;
